62a39509eded21acfdc2a77c45c50c2034688b0d
aliakseis <aliaksei.sanko@gmail.com>
2026-04-08 16:17:17 +0300

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,2 +1,11 @@
-set "PATH=%~dp0gst\bin\;%PATH%"
-"%~dp0\"webrtc-ui.exe
+@echo off
+setlocal
+
+set "GST_ROOT=%~dp0gst"
+
+set "PATH=%GST_ROOT%\bin;%PATH%"
+
+set "GST_PLUGIN_PATH=%GST_ROOT%\lib\gstreamer-1.0"
+set "GST_PLUGIN_SYSTEM_PATH=%GST_ROOT%\lib\gstreamer-1.0"
+
+"%~dp0webrtc-ui.exe"
